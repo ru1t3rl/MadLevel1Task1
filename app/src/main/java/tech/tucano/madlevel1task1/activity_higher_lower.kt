4b056c817dd69e082b5020d5904f75722389e2d4
ru1t3rl <luca.ruiters@hva.nl>
2020-08-31 14:37:18 +0100

    Added UserInput for the lower, equal and higher btns

--- app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
+++ app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
@@ -2,6 +2,7 @@ package tech.tucano.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import tech.tucano.madlevel1task1.databinding.ActivityMainBinding
 
 class activity_higher_lower : AppCompatActivity() {
@@ -16,6 +17,17 @@ class activity_higher_lower : AppCompatActivity() {
         initViews()
     }
 
+    private fun initViews(){
+        updateUI()
+
+        binding.btnLower.setOnClickListener { onLowerClick() }
+        binding.btnEqual.setOnClickListener { onEqualClick() }
+        binding.btnHigher.setOnClickListener { onHigherClick() }
+    }
+
+    /**
+     * Updates the UI based of the variable lastThrow
+     */
     private fun updateUI(){
         binding.result.text = getString(R.string.last_throw, lastThrow)
 
@@ -29,7 +41,58 @@ class activity_higher_lower : AppCompatActivity() {
         }
     }
 
-    private fun initViews(){
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
         updateUI()
+    }
+
+    /**
+     * Rolls the Dice and checks if the throw is higher
+     */
+    private fun onHigherClick(){
+        rollDice()
+
+        if(currentThrow > lastThrow)
+            onCorrect()
+        else
+            onIncorrect()
+    }
+
+    /**
+     * Rolls the Dice and checks if the throw is Lower
+     */
+    private fun onLowerClick(){
+        rollDice()
+
+        if(currentThrow < lastThrow)
+            onCorrect()
+        else
+            onIncorrect()
+    }
+
+    /**
+     * Rolls the Dice and checks if the throw is Equal
+     */
+    private fun onEqualClick(){
+        rollDice()
+        if(currentThrow == lastThrow)
+            onCorrect()
+        else
+            onIncorrect()
+    }
+
+    /**
+     * Displays a correct message
+     */
+    private fun onCorrect(){
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays an incorrect message
+     */
+    private fun onIncorrect(){
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
     }
 }
