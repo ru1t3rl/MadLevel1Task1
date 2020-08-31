19e9f919946f983ee2b4d6ceca62068e7dfdd8ac
ru1t3rl <luca.ruiters@hva.nl>
2020-08-31 14:02:26 +0100

    Created the base layout

--- app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
+++ app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import tech.tucano.madlevel1task1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class activity_higher_lower : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
