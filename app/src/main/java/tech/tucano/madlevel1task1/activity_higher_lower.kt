807ecaa5de1af3b0d331be3602c726d1c53bc9b7
ru1t3rl <luca.ruiters@hva.nl>
2020-08-31 14:23:11 +0100

    Added InitViews (Sets the initial state of the dice)

--- app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
+++ app/src/main/java/tech/tucano/madlevel1task1/activity_higher_lower.kt
@@ -6,10 +6,30 @@ import tech.tucano.madlevel1task1.databinding.ActivityMainBinding
 
 class activity_higher_lower : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun updateUI(){
+        binding.result.text = getString(R.string.last_throw, lastThrow)
+
+        when(currentThrow){
+            1 -> binding.imageDice.setImageResource(R.drawable.dice1)
+            2 -> binding.imageDice.setImageResource(R.drawable.dice2)
+            3 -> binding.imageDice.setImageResource(R.drawable.dice3)
+            4 -> binding.imageDice.setImageResource(R.drawable.dice4)
+            5 -> binding.imageDice.setImageResource(R.drawable.dice5)
+            6 -> binding.imageDice.setImageResource(R.drawable.dice6)
+        }
+    }
+
+    private fun initViews(){
+        updateUI()
     }
 }
